875a202c33c5f38455cc3358cd350c98402b0bc6
Kiktay <drobot.ga@gmail.com>
2025-05-30 13:58:24 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+
+from process import get_prediction
+
+app = Flask(__name__)
+
+@app.route('/', methods=["get", "post"])
+def hello():
+    message = ""
+    if request.method == "POST":
+        area1 = request.form.get("area1")
+        area2 = request.form.get("area2")
+        area3 = request.form.get("area3")
+        area4 = request.form.get("area4")
+        area5 = request.form.get("area5")
+        area6 = request.form.get("area6")
+        area7 = request.form.get("area7")
+        area8 = request.form.get("area8")
+        area9 = request.form.get("area9")
+        area10 = request.form.get("area10")
+        area11 = request.form.get("area11")
+        area12 = request.form.get("area12")
+        try:
+            area1 = float(area1)
+            area2 = float(area2)
+            area3 = float(area3)
+            area4 = float(area4)
+            area5 = float(area5)
+            area6 = float(area6)
+            area7 = float(area7)
+            area8 = float(area8)
+            area9 = float(area9)
+            area10 = int(area10)
+            area11 = float(area11)
+            area12 = float(area12)
+        except Exception as e:
+            print(e)
+            message += "Некорректный ввод."
+
+        cost = get_prediction(area1,area2,area3,area4,area5,area6,area7,area8,area9,area10,area11,area12)
+        message += f"Модуль упругости при растяжении, МПа равна {cost} ."
+    return render_template("index.html", message=message)
+
+if __name__ == '__main__':
+    app.run()
---
+++ process.py
@@ -0,0 +1,11 @@
+import pickle
+import sklearn
+import numpy as np
+
+def get_prediction(area1,area2,area3,area4,area5,area6,area7,area8,area9,area10,area11,area12):
+    with open('model.pkl', 'rb') as f:
+        loaded_model = pickle.load(f)
+
+    data = [area8,area1,area2,area3,area4,area5,area6,area7,area9,area10,area11,area12]
+    y_pred = loaded_model.predict(np.array(data).reshape(1, -1).astype(float))
+    return y_pred
